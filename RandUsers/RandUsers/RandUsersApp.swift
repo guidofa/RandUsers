a8abde1ad273668b54f860e28d4bf0b54398c678
Guido Fabio <54998372+guidofa@users.noreply.github.com>
2025-03-06 18:33:25 -0300

    add: initial setup

---
+++ RandUsers/RandUsers/RandUsersApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandUsersApp.swift
+//  RandUsers
+//
+//  Created by Guido Fabio on 6/3/25.
+//
+
+import SwiftUI
+
+@main
+struct RandUsersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
