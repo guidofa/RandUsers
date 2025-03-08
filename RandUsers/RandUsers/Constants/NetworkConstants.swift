586639b229088887e804808470e2460fb1922bf3
Guido Fabio <gui2_fabio@hotmail.com>
2025-03-08 17:56:08 -0300

    add: NetworkConstants for easier access

---
+++ RandUsers/RandUsers/Constants/NetworkConstants.swift
@@ -0,0 +1,13 @@
+//
+//  NetworkConstants.swift
+//  RandUsers
+//
+//  Created by Guido Fabio on 8/3/25.
+//
+
+import Foundation
+
+struct NetworkConstants {
+    static let apiBaseURLString = "https://randomuser.me/api/"
+    static let usersPerPage = "10"
+}
